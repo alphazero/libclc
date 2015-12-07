6303e63aeda765f6718c93b4e7be2ca716b7b391
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2015-12-07 08:53:56 -0500

    ADD unit-container : drop-in inline.h header

---
+++ include/libclc/inline.h
@@ -0,0 +1,47 @@
+// DOOST
+/* ===-----------------------------------------------------------------------===
+ * Copyright J Muhammad H 2015                                             3.14~
+ * ===--------------------      ~!!! FRIEND !!!~     ------------------------===
+ *                     ~!!! Ever Present Ever Near !!!~
+ *           ~!!! Good Mind Good Expression Good Actualization !!!~
+ *                       ~!!! The REAL The LOVING !!!~
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * This copyright and permission notice shall be included in all copies or any  
+ * portions of this Software.                       
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *===-----------------------------------------------------------------------===
+ */
+
+#ifndef _LIBCLC_INLINE_H_
+#define _LIBCLC_INLINE_H_
+
+#include "libclc.h"
+
+/* NOTE:
+   Uniformly, for all static inline functions defined in headers below, the 
+   function __clc_<something> corresponds to the external linkage function 
+   clc_<something>. Refer to the API header for function documentation.
+ */
+
+#include "__clc_unit.h"
+#include "__clc_stack.h"
+#include "__clc_queue.h"
+#include "__clc_list.h"
+#include "__clc_cache.h"
+
+#endif
